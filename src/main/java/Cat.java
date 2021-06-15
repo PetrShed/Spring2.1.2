2ee6eb459b2cbe41289626c1e6d6bfdbba0a6253
PetrShed <petrshed@gmail.com>
2021-06-15 16:45:05 +0300

    '1'

---
+++ src/main/java/Cat.java
@@ -0,0 +1,30 @@
+public class Cat {
+
+    String name;
+    byte age;
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat named " + name +
+                ", age of " + age;
+    }
+}
